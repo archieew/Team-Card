79b68dd2c4711aed965520fc218cbdedc5104a08
Archie Custodio <archie06custodio@gmail.com>
2025-08-04 20:58:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -185,7 +185,11 @@ const setPlayerCards = (arr = players) => {
 
   playerCards.innerHTML += arr.map(
     ({ name, position, number, isCaptain, nickname }) => {
-
+     return `
+        <div class="player-card">
+      
+        </div>
+      `;
     }
   );
 
