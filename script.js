e0be0c695d01875dfcad24ec87615e84fbbc30aa
Archie Custodio <archie06custodio@gmail.com>
2025-08-04 20:38:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -172,9 +172,10 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
+const { sport, team, year, players } = myFavoriteFootballTeam;
+const { coachName } = myFavoriteFootballTeam.headCoach;
 
+typeOfSport.textContent = sport;
 
-
-const { sport, team } = myFavoriteFootballTeam;
 
 
