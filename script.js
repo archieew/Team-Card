38957ac72295d5f2809b184810db1d0ae6c40349
Archie Custodio <archie06custodio@gmail.com>
2025-08-06 22:18:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -198,6 +198,7 @@ const setPlayerCards = (arr = players) => {
 
 playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
+
   switch (e.target.value) {
     case "nickname":
       setPlayerCards(players.filter((player) => player.nickname !== null));
@@ -215,12 +216,15 @@ playersDropdownList.addEventListener("change", (e) => {
         players.filter((player) => player.position === "defender")
       );
       break;
-
     case "goalkeeper":
       setPlayerCards(
         players.filter((player) => player.position === "goalkeeper")
       );
       break;
+
+  default:
+  setPlayerCards();
+  break;
 
   }
 });
