52cdf667c2675612de94d9f13d4e1808a8c41b52
Archie Custodio <archie06custodio@gmail.com>
2025-08-06 22:03:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -196,8 +196,11 @@ const setPlayerCards = (arr = players) => {
     .join("");
 };
 
-
 playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+
+switch (e.target.value) {
+  
+}
 
 });
-
